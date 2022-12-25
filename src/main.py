54b3b62b579b09125b0075e8364f08aa9b431717
sajidurshajib <sajidur.inbox@gmail.com>
2022-12-26 00:14:50 +0600

    added: sqlalchemy install

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify, request
 from api.v1 import routes
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flaskrepo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.register_blueprint(routes.routes)
+
+
+db = SQLAlchemy()
 
 
 @app.route("/", methods=["GET", "POST"])
